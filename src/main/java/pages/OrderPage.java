c12ddfeb6aa4ffd9c49e4be41955345227e9981a
Nikolai Zablovsky <nickolas.z@yandex.ru>
2025-07-05 16:42:28 +0300

    fixed order test

--- src/main/java/pages/OrderPage.java
+++ src/main/java/pages/OrderPage.java
@@ -1,7 +1,9 @@
 package pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -12,12 +14,8 @@ public class OrderPage {
     private final WebDriverWait wait;
 
     // Локаторы для формы заказа
-    private final By nameField = By.xpath("//input[@placeholder='* Имя']");
-    private final By lastNameField = By.xpath("//input[@placeholder='* Фамилия']");
-    private final By addressField = By.xpath("//input[@placeholder='* Адрес: куда привезти заказ']");
-    private final By metroField = By.xpath("//input[@placeholder='* Станция метро']");
-    private final By phoneField = By.xpath("//input[@placeholder='* Телефон: на него позвонит курьер']");
     private final By nextButton = By.xpath("//button[text()='Далее']");
+    private final By orderButton = By.xpath("(//button[text()='Заказать'])[2]");
     private final By successModal = By.className("Order_ModalHeader");
 
     public OrderPage(WebDriver driver) {
@@ -25,16 +23,89 @@ public class OrderPage {
         this.wait = new WebDriverWait(driver, Duration.ofSeconds(15));
     }
 
+    public WebElement findElementByPlaceholder(String placeholderContent) {
+        String fullContent = String.format("//input[@placeholder='* %s']", placeholderContent);
+        return driver.findElement(By.xpath(fullContent));
+    }
+
+    public void fillCustomerNameField(String name) {
+        WebElement nameField = findElementByPlaceholder("Имя");
+        nameField.sendKeys(name);
+    }
+
+    public void fillCustomerLastNameField(String lastName) {
+        WebElement lastNameField = findElementByPlaceholder("Фамилия");
+        lastNameField.sendKeys(lastName);
+    }
+
+    public void fillCustomerAddressField(String address) {
+        WebElement addressField = findElementByPlaceholder("Адрес: куда привезти заказ");
+        addressField.sendKeys(address);
+    }
+
+    public void fillCustomerMetroField(String metro) {
+        WebElement metroField = findElementByPlaceholder("Станция метро");
+        metroField.sendKeys(metro);
+        WebElement suggestion = wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("//li[@class='select-search__row']")
+        ));
+        suggestion.click();
+    }
+
+    public void fillCustomerPhoneField(String phone) {
+        WebElement phoneField = findElementByPlaceholder("Телефон: на него позвонит курьер");
+        phoneField.sendKeys(phone);
+    }
+
     public void fillCustomerInfo(String name, String lastName, String address, String metro, String phone) {
-        driver.findElement(nameField).sendKeys(name);
-        driver.findElement(lastNameField).sendKeys(lastName);
-        driver.findElement(addressField).sendKeys(address);
-        driver.findElement(metroField).sendKeys(metro);
-        driver.findElement(phoneField).sendKeys(phone);
+        fillCustomerNameField(name);
+        fillCustomerLastNameField(lastName);
+        fillCustomerAddressField(address);
+        fillCustomerMetroField(metro);
+        fillCustomerPhoneField(phone);
+
         driver.findElement(nextButton).click();
     }
 
+    public void fillDateField(String date) {
+        WebElement dateField = findElementByPlaceholder("Когда привезти самокат");
+        dateField.sendKeys(date);
+        dateField.sendKeys(Keys.ESCAPE);
+    }
+
+    public void fillRentRangeField(String rentPeriod) {
+        WebElement rentRangeBlock = driver.findElement(
+                By.xpath("//div[@class='Dropdown-placeholder' and text()='* Срок аренды']")
+        );
+        rentRangeBlock.click();
+        String fullContent = String.format("//div[@class='Dropdown-option' and text()='%s']", rentPeriod);
+        WebElement rentRangeField = driver.findElement(
+                By.xpath(fullContent)
+        );
+        rentRangeField.click();
+    }
+
+    public void fillColorField(String colorId) {
+        WebElement checkbox = driver.findElement(By.id(colorId));
+        checkbox.click();
+    }
+    public void fillCommentField(String comment) {
+        WebElement fullContent = driver.findElement(By.xpath("//input[@placeholder='Комментарий для курьера']"));
+        fullContent.sendKeys(comment);
+    }
+
+    public void fillRentInfo(String date, String rentPeriod, String colorId, String comment) {
+        fillDateField(date);
+        fillRentRangeField(rentPeriod);
+        fillColorField(colorId);
+        fillCommentField(comment);
+
+        WebElement orderButtonElement = driver.findElement(orderButton);
+        orderButtonElement.click();
+    }
+
     public boolean isOrderConfirmed() {
-        return wait.until(ExpectedConditions.visibilityOfElementLocated(successModal)).isDisplayed();
+        By yesButton = By.xpath("//button[text()='Да']");
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(yesButton)).isDisplayed();
     }
 }
--- src/test/java/tests/OrderTest.java
+++ src/test/java/tests/OrderTest.java
@@ -20,22 +20,30 @@ public class OrderTest extends BaseTest {
     private final String metro;
     private final String phone;
     private final boolean useTopButton;
+    private final String date;
+    private final String rentPeriod;
+    private final String colorId;
+    private final String comment;
 
     public OrderTest(String name, String lastName, String address,
-                     String metro, String phone, boolean useTopButton) {
+                     String metro, String phone, boolean useTopButton, String date, String rentPeriod, String colorId, String comment) {
         this.name = name;
         this.lastName = lastName;
         this.address = address;
         this.metro = metro;
         this.phone = phone;
         this.useTopButton = useTopButton;
+        this.date = date;
+        this.rentPeriod = rentPeriod;
+        this.colorId = colorId;
+        this.comment = comment;
     }
 
     @Parameterized.Parameters
     public static Collection<Object[]> data() {
         return Arrays.asList(new Object[][]{
-                {"Иван", "Иванов", "ул. Ленина, 1", "Черкизовская", "+79998887766", true},
-                {"Петр", "Петров", "ул. Пушкина, 10", "Сокольники", "+79997776655", false}
+                {"Иван", "Иванов", "ул. Ленина, 1", "Черкизовская", "+79998887766", true, "20.06.2025", "сутки", "black", "123Abcd*&,"},
+//                {"Петр", "Петров", "ул. Пушкина, 10", "Сокольники", "+79997776655", false, "20.12.2025", "пятеро суток", "gray", ""}
         });
     }
 
@@ -52,9 +60,7 @@ public class OrderTest extends BaseTest {
 
         OrderPage orderPage = new OrderPage(driver);
         orderPage.fillCustomerInfo(name, lastName, address, metro, phone);
-
-        // Здесь можно добавить заполнение второй страницы формы
-        // orderPage.fillRentInfo(...);
+        orderPage.fillRentInfo(date, rentPeriod, colorId, comment);
 
         assertTrue("Заказ не подтвержден", orderPage.isOrderConfirmed());
     }
